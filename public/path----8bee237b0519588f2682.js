4d65d8b545c97e4b58d5dfc56cfee9edff2eb858
Fabricio Anciães <Fabricio4p93@gmail.com>
2018-04-17 22:45:17 -0300

    fazendo a build do site

---
+++ public/path----8bee237b0519588f2682.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{75:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Fighthub"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----8bee237b0519588f2682.js.map
