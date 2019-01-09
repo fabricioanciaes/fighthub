a59f4f5112ab4dd57a3edf09ba48c7d0638603a6
BOBS\Fabricio.Pereira <Fabricio.pereira@bffc.com.br>
2019-01-09 09:42:27 -0200

    Instalação do axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+/*
+* Coloca o axios como plugin no vue
+* Basicamente estou colocando o axios como um método na instância do vue
+* agora é possível fazer this.$api.get('/endpoint') e etc.
+*/
+
+Vue.use({
+  install (Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: 'http://fighthub-api.herokuapp.com'
+    })
+  }
+})
 
 new Vue({
   render: h => h(App),
