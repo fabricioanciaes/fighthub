4e7cc87069fc0fd0eb8ae32f1c90088bd1271003
BOBS\Fabricio.Pereira <Fabricio.pereira@bffc.com.br>
2019-01-10 15:11:48 -0200

    Componente do evento e requests de api usando fetch (desisti do axios)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,54 @@
 <template>
-  <div class="home"><h1>Home</h1></div>
+  <div class="home">
+    <div v-if="loading">Carregando...</div>
+    <div v-if="error">
+      Erro! Não conseguimos nos comunicar com o servidor, por favor tente de
+      novo mais tarde.
+    </div>
+
+    <div class="wrapper">
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 
+import EventCard from "@/components/EventCard.vue";
+
 export default {
   name: "home",
-  components: {}
+  components: { EventCard },
+  data() {
+    return {
+      events: [],
+      loading: false,
+      error: false,
+      apiUrl: "http://fighthub-api.herokuapp.com/events"
+    };
+  },
+  methods: {
+    fetchEvents: function() {
+      this.loading = true;
+      fetch(this.apiUrl)
+        .then(response => response.json()) // retorna uma promise
+        .then(result => {
+          console.log(result);
+          this.loading = false;
+          this.events = result;
+        })
+        .catch(err => {
+          // trata se alguma das promises falhar
+          console.error("Failed retrieving information", err);
+          this.loading = false;
+          this.error = true;
+        });
+    }
+  },
+  mounted() {
+    return this.fetchEvents();
+  }
 };
 </script>
